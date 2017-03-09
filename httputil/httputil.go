f130452f608e67517e6dc1c62b0aef6fb95303de
plumhj <plumhj@gmail.com>
2017-03-09 15:17:16 +0900

    dd

---
+++ httputil/httputil.go
@@ -0,0 +1,25 @@
+package httputil
+
+import (
+	"net/http"
+	"bytes"
+	"io/ioutil"
+)
+
+func PostJson(url string, payload []byte) (response []byte, err error) {
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	resp, err = ioutil.ReadAll(resp.Body)
+	return
+}
